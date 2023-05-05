0bccdf4196a893e001e1d1be9dd85e9d0867966c
ManuelMadriz <00026220@gmail.com>
2023-05-04 18:42:43 -0600

    init project

---
+++ src/main/java/com/grupo4/taller2/Taller2Application.java
@@ -0,0 +1,13 @@
+package com.grupo4.taller2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Taller2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Taller2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo4/taller2/Taller2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo4.taller2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Taller2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
